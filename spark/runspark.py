54575b87250fa73770e7130cfcf1529c5b4b0cf4
DPSQUARE <ppd@PPDs-MacBook-Pro.local>
2020-01-15 16:07:52 -0500

    first attempt at the word count

--- spark/runspark.py
+++ spark/runspark.py
@@ -1,1 +1,21 @@
-print("need to connect to s3 and run the pyspark")
+import sys
+ 
+from pyspark import SparkContext, SparkConf
+ 
+if __name__ == "__main__":
+
+    print ('Number of arguments:', len(sys.argv), 'arguments.')
+    print ('Argument List:', str(sys.argv))
+
+    #create Spark context with necessary configuration
+    sc = SparkContext("local","PySpark Word Count Exmaple")
+
+    #read data from text file and split each line into words
+    words = sc.textFile(sys.argv[1]).flatMap(lambda line: line.split(" "))
+
+    #count the occurence of each word
+    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b)
+
+    wordCounts.saveAsTextFile(sys.argv[2])
+
+
